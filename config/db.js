6676922a204e26040fef79fa07ae4dc6ab648512
Omar Ontiveros <omar.ontiveros@alumnos.udg.mx>
2022-10-04 21:42:09 -0500

    agrega ssl config

--- config/db.js
+++ config/db.js
@@ -1,5 +1,7 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env['DATABASE_URL']);
+const sequelize = new Sequelize(process.env['DATABASE_URL'],  {dialectOptions: {
+    ssl: {require:true, rejectUnauthorized: false }
+}});
 
 module.exports = sequelize;
